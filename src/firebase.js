d0ab8c4f5e3fd82219f33f40dcc3c32f29c9c53f
nidhimistry275 <mistrynidhi94@gmail.com>
2023-12-23 21:31:49 -0700

    Sign Up pageauthentication

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCcKC0p7PjT5JGkAHFlpo_o5nlf-K-6V7M",
   authDomain: "netflix-e6a1a.firebaseapp.com",
@@ -6,3 +10,10 @@ const firebaseConfig = {
   messagingSenderId: "496937200645",
   appId: "1:496937200645:web:b876a6f61b47f41390806c",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth };
+export default db;
